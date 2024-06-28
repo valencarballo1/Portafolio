58264b319dc80347733e66c91eba3fbb52fc06c3
Valentin Carballo <valentincarballo99@gmail.com>
2024-06-28 13:30:41 -0300

    portfolio valen

---
+++ script.js
@@ -0,0 +1,29 @@
+const descargaCv = document.getElementById("descargaCv");
+
+descargaCv.addEventListener("click", function(){
+    descargaCv.addEventListener("click", function(){
+        const pdfPath = "../assets/FullStack_Valentin_Carballo.pdf"; // Ruta ajustada
+        window.open(pdfPath, '_blank');
+    });
+});
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive: {
+            0: {
+                items: 2 // Mostrar 1 elemento en pantallas pequeñas (menos de 576px)
+            },
+            768: {
+                items: 4 // Mostrar 3 elementos en pantallas medianas (de 576px a 992px)
+            },
+            992: {
+                items: 6 // Mostrar 4 elementos en pantallas grandes (992px o más)
+            }
+        }
+    });
+});
