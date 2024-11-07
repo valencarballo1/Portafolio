8d2e20d33639d9e8ac35a1f29c61ee99dcf88cc2
Valentin Carballo <valentincarballo99@gmail.com>
2024-11-07 16:06:49 -0300

    cambio estilos

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        nav: false,
-        dots: false,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        responsive: {
-            0: {
-                items: 2 // Mostrar 1 elemento en pantallas pequeñas (menos de 576px)
-            },
-            768: {
-                items: 4 // Mostrar 3 elementos en pantallas medianas (de 576px a 992px)
-            },
-            992: {
-                items: 6 // Mostrar 4 elementos en pantallas grandes (992px o más)
-            }
-        }
-    });
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.querySelector('nav ul');
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('flex-col');
+    menu.classList.toggle('absolute');
+    menu.classList.toggle('top-16');
+    menu.classList.toggle('left-0');
+    menu.classList.toggle('right-0');
+    menu.classList.toggle('bg-white');
+    menu.classList.toggle('dark:bg-gray-800');
+    menu.classList.toggle('shadow-md');
+    menu.classList.toggle('p-4');
 });
